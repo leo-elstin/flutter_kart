f6665d6c85db026569b797a51f63f5f93ecb3cdf
Leo Elstin <leo.elstin@azinova.info>
2019-01-24 12:14:47 +0530

    ** Committed By Leo Elstin **

--- lib/home/product_details.dart
+++ lib/home/product_details.dart
@@ -12,8 +12,17 @@ class ProductDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text('Comming soon'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network('https://flutter.io/assets/homepage/news-2-599aefd56e8aa903ded69500ef4102cdd8f988dab8d9e4d570de18bdb702ffd4.png'),
+          Text('Comming soon'),
+          RaisedButton(
+            child: Text('Go Back'),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
       ),
     );
   }
