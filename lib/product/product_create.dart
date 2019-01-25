27ba287cce1f0cb9c3ae8fa7d6675542eff07300
Leo Elstin <leo.elstin@azinova.info>
2019-01-25 19:02:11 +0530

    ** Committed By Leo Elstin **

--- lib/product/product_create.dart
+++ lib/product/product_create.dart
@@ -2,9 +2,36 @@ import 'package:flutter/material.dart';
 
 class ProductCreate extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Text('Product Create'),
-      );
-    }
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Show Dialog'),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Are you sure ?'),
+                  content: Text('The action is non recoverable.'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Yes, Delete.'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        // add your action here .
+                      },
+                    )
+                  ],
+                );
+              });
+        },
+      ),
+    );
+  }
 }
